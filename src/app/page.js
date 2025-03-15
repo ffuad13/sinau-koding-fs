20b3483ba53cc5b0f387921d1218d028f81dc3d6
ffuad13 <jhonatom99@gmail.com>
2025-03-15 10:09:30 +0800

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,79 @@
-import Image from "next/image";
+import React from "react";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+const App = () => {
+	// return React.createElement("h1", null, "Hello, no JSX") //tanpa jsx
+	// return <h1>Hello, saya JSX</h1> //dengan jsx
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	// const nama = 'Faizul'
+	// const umur = '17'
+
+	// const calculateSum = (a, b) => a + b
+	const UserStatus = ({isLoggedIn}) => {
+		return (
+			<div>
+				{isLoggedIn && <p>Selamat datang, user!</p>}
+				{!isLoggedIn && <p>Silahkan Login</p>}
+			</div>
+		)
+	}
+
+	const UserList = ({users}) => {
+		return (
+			<ul>
+				{users.length > 0 ? (
+					users.map((user, index) => <li key={index}>{user}</li>)
+				) : (
+					<p>User not found</p>
+				)}
+			</ul>
+		)
+	}
+
+	const statusFromDB = false
+	const dataUserFromDB = ['Aris', 'Budi', 'Joko', 'Santoso', 'Helmi']
+
+	//components greeting
+	const Greetings = (props) => {
+		const myArray = ['Sepatu', 'tas', 'dompet', 'Topi']
+		const [a, ...sisanya] = myArray
+		const {name, age, hobby} = props
+		return (
+			<>
+		<h1>Halo {name}, selamat datang</h1>
+		<p>Hobi anda adalah {hobby}</p>
+		<p>Umur anda {age}</p>
+		<p>Anda senang mengoleksi {a} dan sudah memiliki {sisanya}</p>
+			</>
+	)
+	}
+
+	return (
+		<>
+		{/* singe child */}
+		<h1>Single child element</h1>
+
+		{/* fragment jsx */}
+		<>
+			<h2>Using fragment</h2>
+			<p>this is inside react fragment</p>
+		</>
+
+		<div>
+			<br></br>
+			<h2>Using div</h2>
+			<p>I am using div</p>
+			<br></br>
+		</div>
+		<div>
+			<Greetings name="Faizul" age="23" hobby="Membaca"/>
+		</div>
+		<div>
+			<UserStatus isLoggedIn={statusFromDB}/>
+			<h2>User List</h2>
+			<UserList users={dataUserFromDB}/>
+		</div>
+		</>
+	)
 }
+
+export default App
